76a1cfaca84a4806806022ff8ad73bf9ce607422
Jason O'Neil <jason.oneil@gmail.com>
2018-02-24 16:09:30 +0800

    Improve deploy script

--- deployEnthralerDotCom.sh
+++ deployEnthralerDotCom.sh
@@ -1,6 +1,7 @@
 yarn run webpack -- -p
-rsync -av bin/lib/ do:/var/www/enthraler/lib/
+rsync -av bin/package.json do:/var/www/enthraler/package.json
+rsync -av bin/yarn.lock do:/var/www/enthraler/yarn.lock
+ssh do "cd /var/www/enthraler; yarn"
 rsync -av bin/assets/ do:/var/www/enthraler/assets/
-rsync -av bin/res/ do:/var/www/enthraler/res/
-rsync -av bin/index.php do:/var/www/enthraler/index.php
-ssh do "cd /var/www/enthraler; php index.php"
+rsync -av bin/server.js.map do:/var/www/enthraler/server.js.map
+rsync -av bin/server.js do:/var/www/enthraler/server.js
