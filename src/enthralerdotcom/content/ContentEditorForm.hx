596fc031fbf5b94faa9f21570e04a46957963eff
Jason O'Neil <jason.oneil@gmail.com>
2018-05-12 09:36:57 +0200

    Get Shape proptypes working

--- src/enthralerdotcom/content/ContentEditorForm.hx
+++ src/enthralerdotcom/content/ContentEditorForm.hx
@@ -13,35 +13,38 @@ typedef ContentEditorFormProps<Content> = {
 class ContentEditorForm<Content> extends UniversalComponent<ContentEditorFormProps<Content>, {}> {
 	override public function render() {
 		var fields = [for (name in props.schema.keys()) {
-			var propType = props.schema.get(name);
-			var type = propType.getEnum();
 			var value: Dynamic = Reflect.field(props.content, name);
+			var type = props.schema.get(name).getEnum();
 			var onChange = function (newVal: Dynamic) {
 				// In JavaScript I would usually try clone props.content rather than modify it directly.
 				// However I'm not sure the easiest way to treat objects as immutable in Haxe.
 				Reflect.setField(props.content, name, newVal);
 				props.onChange(props.content);
 			};
-			var input = switch type {
-				case PTArray(optional): jsx('<span>Array (multiple inputs)</span>');
-				case PTBool(optional): jsx('<Checkbox value=${value} onChange=${onChange} />');
-				case PTNumber(optional): jsx('<IntInput value=${value} onChange=${onChange} />');
-				case PTInteger(optional): jsx('<FloatInput value=${value} onChange=${onChange} />');
-				case PTObject(optional): jsx('<textarea></textarea>');
-				case PTString(optional): jsx('<TextInput value=${value} onChange=${onChange}></TextInput>');
-				case PTOneOf(values, optional): jsx('<SelectInput options=${values} value=${value} onChange=${onChange}></SelectInput>');
-				case PTOneOfType(types, optional): jsx('<span>TODO: one of type...</span>');
-				case PTArrayOf(subType, optional): jsx('<span>TODO: Array (multiple inputs)</span>');
-				case PTObjectOf(subType, optional): jsx('<span>TODO: Object (multiple inputs)</span>');
-				case PTShape(shapedObject, optional): jsx('<span>TODO: Shape</span>');
-				case PTAny(optional): jsx('<textarea></textarea>');
-			};
-			jsx('<div key=${name}><label>${name}: ${input}</label></div>');
+			renderField(name, type, value, onChange);
 		}];
 		return jsx('<div>
 			<h2>Form</h2>
 			<div>${fields}</div>
 		</div>');
+	}
+
+	static function renderField(name: String, type: PropTypeEnum, value: Dynamic, onChange: Dynamic->Void) {
+		var input = switch type {
+			case PTArray(optional): jsx('<span>Array (multiple inputs)</span>');
+			case PTBool(optional): jsx('<Checkbox value=${value} onChange=${onChange} />');
+			case PTNumber(optional): jsx('<IntInput value=${value} onChange=${onChange} />');
+			case PTInteger(optional): jsx('<FloatInput value=${value} onChange=${onChange} />');
+			case PTObject(optional): jsx('<textarea></textarea>');
+			case PTString(optional): jsx('<TextInput value=${value} onChange=${onChange}></TextInput>');
+			case PTOneOf(values, optional): jsx('<SelectInput options=${values} value=${value} onChange=${onChange}></SelectInput>');
+			case PTOneOfType(types, optional): jsx('<span>TODO: one of type...</span>');
+			case PTArrayOf(subType, optional): jsx('<span>TODO: Array (multiple inputs)</span>');
+			case PTObjectOf(subType, optional): jsx('<span>TODO: Object (multiple inputs)</span>');
+			case PTShape(shapedObject, optional): jsx('<ShapeForm name=${name} shape=${shapedObject} value=${value} onChange=${onChange} />');
+			case PTAny(optional): jsx('<textarea></textarea>');
+		};
+		return jsx('<div key=${name}><label>${name}: ${input}</label></div>');
 	}
 
 	static function TextInput(props: {value: String, onChange: String->Void}) {
@@ -63,12 +66,30 @@ class ContentEditorForm<Content> extends UniversalComponent<ContentEditorFormPro
 	}
 
 	static function IntInput(props: {value: Int, onChange: Int->Void}) {
-		var onChange = function (e) props.onChange(Math.round(e.target.valueAsNumber));
+		var onChange = function (e) props.onChange(Math.round(e.target.valueAsNumber ));
 		return jsx('<input type="number" step={1} value=${props.value} onChange=${onChange} />');
 	}
 
 	static function FloatInput(props: {value: Float, onChange: Float->Void}) {
 		var onChange = function (e) props.onChange(e.target.valueAsNumber);
 		return jsx('<input type="number" step={1} value=${props.value} onChange=${onChange} />');
+	}
+
+	static function ShapeForm(props: {name: String, shape: Dynamic<PropTypeEnum>, value: Dynamic, onChange: Dynamic->Void}) {
+		var shapeValue = props.value;
+		var fields = [for (name in Reflect.fields(props.shape)) {
+			var type = Reflect.field(props.shape, name);
+			var fieldValue = Reflect.field(shapeValue, name);
+			var onChange = function (newFieldVal: Dynamic) {
+				Reflect.setField(shapeValue, name, newFieldVal);
+				props.onChange(shapeValue);
+			};
+			renderField(name, type, fieldValue, onChange);
+		}];
+		return jsx('<div>
+			<fieldset name=${props.name}>
+				${fields}
+			</fieldset>
+		</div>');
 	}
 }
